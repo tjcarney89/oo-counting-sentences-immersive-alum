63cf538828132904549f95bc9e5b34e60903ced0
TJ Carney <tjcarney@stny.rr.com>
2017-11-13 18:20:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    split_up = self.split(/[.!?]/)
+    sentences = []
+    split_up.each do |string|
+      if string != ""
+        sentences << string
+      end
+    end
+    sentences.count
   end
 end
